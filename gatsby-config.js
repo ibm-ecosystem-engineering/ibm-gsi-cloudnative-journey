199f677882ff21dae66bbab0581bf88449e8c8c9
Sean Sundberg <seansund@us.ibm.com>
2021-10-25 07:18:11 -0500

    Adds gatsby-plugin-sharp back
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,20 @@ module.exports = {
         head:true
       },
     },
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        // Defaults used for gatsbyImageData and StaticImage
+        defaults: {},
+        // Set to false to allow builds to continue on image errors
+        failOnError: true,
+        // deprecated options and their defaults:
+        base64Width: 20,
+        forceBase64Format: `png`, // valid formats: png,jpg,webp
+        useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
+        stripMetadata: true,
+        defaultQuality: 50,
+      },
+    },
   ]
 };
